681d116801ba339e09c62d8f027ee263891db21f
Suchakree <porlolicon@gmail.com>
2019-09-08 19:04:34 +0700

    add deptlimitsearch

--- src/components/TreeView/TreeView.jsx
+++ src/components/TreeView/TreeView.jsx
@@ -1,47 +1,11 @@
 import React, { Component } from 'react'
 import Node from './Node'
-import Tree from 'react-d3-tree';
+import Tree from 'react-d3-tree'
+import { createRoot, INIT_GAME_STATE } from './treeUtil'
+import { deptLimitSearch, MAX_VISUALIZE_DEPTH, BOARD_SYMBOL, delay } from './gameUtil'
 
-const myTreeData = {
-  name: "1",
-  children: [
-    {
-      name: "2",
-      yay: '66'
-    },
-    {
-      name: "2",
-      children: [
-        {
-          name: "2",
-          yay: '66'
-        },
-        {
-          name: "2",
-          children: [
-            {
-              name: "2",
-              yay: '66'
-            },
-            {
-              name: "2",
-              children: [
-                {
-                  name: "2",
-                  yay: '66'
-                },
-                {
-                  name: "2"
-                }
-              ]
-            }
-          ]
-        }
-
-      ]
-    }
-  ]
-};
+// Maybe pass this from Context API?
+const gameState = createRoot(INIT_GAME_STATE)
 
 export default class TreeView extends Component {
 
@@ -59,23 +23,62 @@ export default class TreeView extends Component {
         x: dimensions.width / 2,
         y: 50
       }
-    });
+    })
+
+    setTimeout(async () => {
+      const search = deptLimitSearch({
+        root: gameState[0],
+        limit: MAX_VISUALIZE_DEPTH,
+        turn: BOARD_SYMBOL.PLAYER
+      })
+
+      console.time('time used')
+      
+      let next = search.next()
+      let data = next.value
+
+      let i = 1
+
+      while (!next.done) {
+        next = search.next()
+        if (next.value) {
+          i++
+          data = next.value
+          this.tree.setState({ data: data })
+          await delay(50)
+        }
+      }
+
+      console.log(`Searched ${i} possible ways`)
+      console.timeEnd('time used')
+
+    }, 50)
+
   }
 
   render() {
     return (
       <div ref={tc => (this.treeContainer = tc)} style={{ height: '100vh' }}>
         <Tree
-          data={myTreeData}
+          ref={e => this.tree = e}
+          pathFunc='elbow'
+          zoom={0.5}
+          data={gameState}
           translate={this.state.translate}
           orientation='vertical'
+          useCollapseData
           allowForeignObjects
+          nodeSize={{
+            x: 240,
+            y: 260
+          }}
+          transitionDuration={0}
           nodeLabelComponent={{
-            render: <Node />,
+            render: <Node width={200} height={200} />,
             foreignObjectWrapper: {
-              y: -45,
-              x: -55
-            }
+              x: -100,
+              y: -110
+            },
           }}
         />
       </div>
