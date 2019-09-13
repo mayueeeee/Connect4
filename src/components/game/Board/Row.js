8d7ab89705487bb76cff9395bce97ab2675f1375
Sarunyu Chankong <sarunyu.you@gmail.com>
2019-09-13 12:56:58 +0700

    Add hover with bug

--- src/components/game/Board/Row.js
+++ src/components/game/Board/Row.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Dot from '../Dot'
 import isEmpty from 'lodash/isEmpty'
 import PropTypes from 'prop-types'
 
 const Row = props => {
+
+    useEffect(()=>{
+        console.log('yay')
+    })
+
+
     return (
-        <div className="column">
+        <div className="column game-row">
             {!isEmpty(props.data) &&
                 props.data.map((color, i) => (
-                    <div className="columns" onMouseOver={props.hover}>
+                    <div className="columns" onClick={props.hover}>
                         <div className="column is-12">
                             <Dot key={i} color={color} />
                         </div>
