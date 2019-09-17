306c6796363f0a3433f446b8822876e835e49c5b
Sarunyu Chankong <sarunyu.you@gmail.com>
2019-09-17 13:59:15 +0700

    Remove unused lib

--- src/components/game/Board/Board.js
+++ src/components/game/Board/Board.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import lastIndexOf from 'lodash/lastIndexOf'
 import Row from './Row'
-import Dot from '../Dot'
 
 export default class Board extends React.Component {
     state = {
         board: Array(7)
             .fill(0)
             .map(x => Array(6).fill('blank')),
-
         turn: 0,
     }
 
     onRowClick = (rowNum) => {
-        const color = this.state.turn == 0 ? 'red' : 'green'
+        const color = this.state.turn === 0 ? 'red' : 'green'
         console.log('Row ', rowNum, ' Color ', color)
         const lastBlank = lastIndexOf(this.state.board[rowNum], 'blank')
         let tmp = this.state.board
--- src/components/game/Board/Row.js
+++ src/components/game/Board/Row.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Dot from '../Dot'
 import isEmpty from 'lodash/isEmpty'
 import PropTypes from 'prop-types'
