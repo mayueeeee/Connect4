ae59358b83f33ca497f249915dfa421a667d664d
Sarunyu Chankong <sarunyu.you@gmail.com>
2019-09-17 19:43:25 +0700

    Fix array diamension

--- src/components/game/Board/Board.js
+++ src/components/game/Board/Board.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import lastIndexOf from 'lodash/lastIndexOf'
 import Row from './Row'
 
 export default class Board extends React.Component {
 
-  onRowClick = rowNum => {
-    this.props.onRowClick(rowNum)
-  }
+    onRowClick = rowNum => {
+        this.props.onRowClick(rowNum)
+    }
 
-  render() {
-    const { board } = this.props
+    render() {
+        const { board } = this.props
+        const yay = board[0].map((col, i) => board.map(row => row[i]));
 
-    return (
-      <div className="game-board">
-        <div className="columns is-centered">
-          {board.map((row, i) => (
-            <Row key={i} data={row} onClick={e => this.onRowClick(i)} />
-          ))}
-        </div>
-      </div>
-    )
-  }
+        return (
+            <div className="game-board">
+                <div className="columns is-centered">
+                    {yay.map((row, i) => (
+                        <Row key={i} data={row} onClick={e => this.onRowClick(i)}/>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
