21a78244f915ed47d816ab0c7571853a869948f2
Sarunyu Chankong <sarunyu.you@gmail.com>
2019-11-25 23:14:33 +0700

    Add key and average

--- src/components/StackTable/StackTable.js
+++ src/components/StackTable/StackTable.js
@@ -14,24 +14,34 @@ export default props => (
     <tbody>
       {!isNull(props.trace) &&
         props.trace.map((ele, index) => (
-          <tr>
+          <tr key={index}>
             <td>{index + 1}</td>
             <td>{ele.maxStack}</td>
             <td>{ele.timeUsed} ms</td>
           </tr>
         ))}
-
-      <tr>
-        <td>
-          <b>avg</b>
-        </td>
-        <td>
-          <b>2</b>
-        </td>
-        <td>
-          <b>2 ms</b>
-        </td>
-      </tr>
+      {!isNull(props.trace) && props.winner && (
+        <tr>
+          <td>
+            <b>avg</b>
+          </td>
+          <td>
+            <b>
+              {props.trace.reduce((total, next) => total + next.maxStack, 0) /
+                props.trace.length}
+            </b>
+          </td>
+          <td>
+            <b>
+              {(
+                props.trace.reduce((total, next) => total + next.timeUsed, 0) /
+                props.trace.length
+              ).toFixed(2)}{' '}
+              ms
+            </b>
+          </td>
+        </tr>
+      )}
     </tbody>
   </Table>
 )
